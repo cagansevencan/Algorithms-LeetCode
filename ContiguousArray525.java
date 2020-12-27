45c12531731aa91fec095510d1c3f807bf31ed87
Cagan Sevencan <cagansvncn@gmail.com>
2020-12-26 22:43:15 -0800

    Comments

--- ContiguousArray525.java
+++ ContiguousArray525.java
@@ -1,6 +1,9 @@
 import java.util.Arrays;
 import java.util.HashMap;
 
+//Time complexity : O(n) The entire array is traversed only once.
+//Space complexity : O(n). Maximum size of the HashMap map will be \text{n}n, if all the elements are either 1 or 0.
+
 public class ContiguousArray525 {
     public static int findMaxLength(int[] nums) {
         HashMap<Integer, Integer> map = new HashMap<>();
@@ -20,7 +23,7 @@ public class ContiguousArray525 {
 
 
     public static void main(String[] args) {
-        int[] array = {0, 0, 1, 0, 1, 0, 0, 0, 1, 0};
+        int[] array = {0, 0, 1, 0, 0, 0, 1, 1};
         int n = findMaxLength(array);
         System.out.println(n);
     }
