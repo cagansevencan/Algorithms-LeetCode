bfa49168c4d4d00f2d24851e1f9e89e9129e66c1
Cagan Sevencan <cagansvncn@gmail.com>
2020-12-26 20:00:35 -0800

    Create ContiguousArray525.java

---
+++ ContiguousArray525.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class ContiguousArray525 {
+    public static int findMaxLength(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0,-1);
+        int max = 0;
+        int count = 0;
+        for(int i =0; i < nums.length; i++){
+            count+= (nums[i] == 1 ? 1 : -1);
+            if(map.containsKey(count)){
+                max = Math.max(max, i-map.get(count));
+            }else{
+                map.put(count, i);
+            }
+        }
+        return max;
+    }
+
+
+    public static void main(String[] args) {
+        int[] array = {0, 0, 1, 0, 1, 0, 0, 0, 1, 0};
+        int n = findMaxLength(array);
+        System.out.println(n);
+    }
+
+}
