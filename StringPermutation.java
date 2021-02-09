869cc158b1f837b042b48187fb6eb43bc4a72faf
Cagan Sevencan <cagansvncn@gmail.com>
2021-02-08 21:11:54 -0800

    N Time Complexity

--- StringPermutation.java
+++ StringPermutation.java
@@ -3,24 +3,45 @@ import java.util.HashSet;
 
 public class StringPermutation {
 
-     static String sort(String s){
+
+    static String sort(String s) {
         char[] content = s.toCharArray();
         Arrays.sort(content);
         return new String(content);
 
     }
 
-    public static boolean isPermutation(String word, String word2){
+    //NlogN Version
+    public static boolean isPermutation(String word, String word2) {
         return sort(word).equals(sort(word2));
     }
 
-    public static void main(String[] args) {
-        String word1 = "";
-        String word2 = " ";
-        Boolean t = isPermutation(word1, word2);
-        System.out.println(t);
+
+    //N running time
+    public static boolean permutation(String word, String word2) {
+        if (word.length() != word2.length()) {
+            return false;
+        }
+
+        int[] letters = new int[128];
+        for (int i = 0; i < word.length(); i++) {
+            letters[word.charAt(i)]++;
+        }
+
+        for (int i = 0; i < word2.length(); i++) {
+            letters[word2.charAt(i)]--;
+            if (letters[word2.charAt(i)] < 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
 
-    
+    public static void main(String[] args) {
+        String word1 = "doge";
+        String word2 = "goda";
+        Boolean t = permutation(word1, word2);
+        System.out.println(t);
+    }
 }
