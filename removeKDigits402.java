c7f34ed9bbac0d017fd7e9ef3cbc846eebdda234
Cagan Sevencan <36283663+cagansevencan@users.noreply.github.com>
2021-03-12 23:03:15 -0800

    Create removeKDigits402.java

---
+++ removeKDigits402.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class removeKDigits402 {
+    public static void removeKdigits(String num, int k) {
+
+    }
+
+    public static void main(String[] args) {
+        removeKdigits("1432219", 3);
+    }
+
+
+}
