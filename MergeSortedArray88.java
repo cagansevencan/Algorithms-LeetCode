29db9c831fe2ff2dcdc203c22689ce3415ef2328
Cagan <cagansvncn@gmail.com>
2021-06-02 16:16:22 -0700

    Update MergeSortedArray88.java

--- MergeSortedArray88.java
+++ MergeSortedArray88.java
@@ -2,18 +2,19 @@ import java.util.Arrays;
 
 public class MergeSortedArray88 {
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
-        int[] nums1Copy = new int[m];
-        for(int i = 0; i < m; i++){
-            nums1Copy[i] = nums1[i];
-        }
-        int p1 =0, p2 = 0;
+
+        int p1 =m-1, p2 = n-1;
 
         //Compare elements
-        for (int p = 0; p < m+n; p++){
-            if(p2 >= n || (p1 < m && nums1Copy[p1] < nums2[p2])){
-                nums1[p] = nums1Copy[p1++];
+        for (int p = m+n-1; p >= 0 ; p--){
+            if(p2< 0){
+                break;
+            }
+
+            if(p1 >= 0 && (nums1[p1] > nums2[p2])){
+                nums1[p] = nums1[p1--];
             }else{
-                nums1[p] = nums2[p2++];
+                nums1[p] = nums2[p2--];
             }
         }
     }
@@ -24,6 +25,7 @@ public class MergeSortedArray88 {
     int n = 3;
     int m = 3;
     merge(array1, m, array2, n);
-    Arrays.toString(array1);
+
+    System.out.println(Arrays.toString(array1));
     }
 }
