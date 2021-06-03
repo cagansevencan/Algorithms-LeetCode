d2859955ab8401135cbcccfae21a409816289a2d
Cagan <cagansvncn@gmail.com>
2021-06-03 14:16:24 -0700

    Create LRUCache146.java

---
+++ LRUCache146.java
@@ -0,0 +1,16 @@
+public class LRUCache146 {
+    class LRUCache {
+        int size = 0;
+        public LRUCache(int capacity) {
+            size = capacity;
+        }
+
+        public int get(int key) {
+
+        }
+
+        public void put(int key, int value) {
+
+        }
+    }
+}
