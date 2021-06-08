3ec88edfc359a3f8a270a7ac548292034a5ee900
Cagan <cagansvncn@gmail.com>
2021-06-08 12:54:12 -0700

    Create LinkedListCycle141.java

---
+++ LinkedListCycle141.java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class LinkedListCycle141 {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+
+    public boolean hasCycle(ListNode head) {
+        
+    }
+}
