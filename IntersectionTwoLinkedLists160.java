001deb0cec0fcc4a90abe13d2904f53db5bd61df
Cagan <cagansvncn@gmail.com>
2021-06-07 18:24:45 -0700

    Create IntersectionTwoLinkedLists160.java

---
+++ IntersectionTwoLinkedLists160.java
@@ -0,0 +1,17 @@
+
+// Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class IntersectionTwoLinkedLists160 {
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+
+    }
+}
