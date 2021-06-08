7aa35308420ab98efc3f1e5e40f16f904e0008b7
Cagan <cagansvncn@gmail.com>
2021-06-07 20:23:46 -0700

    Update IntersectionTwoLinkedLists160.java

--- IntersectionTwoLinkedLists160.java
+++ IntersectionTwoLinkedLists160.java
@@ -11,7 +11,48 @@ class ListNode {
 
 public class IntersectionTwoLinkedLists160 {
 
-    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+    public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode pA = headA;
+        ListNode pB = headB;
+        while (pA != pB) {
+            pA = pA == null ? headB : pA.next;
+            pB = pB == null ? headA : pB.next;
+        }
+        return pA;
+    }
+
+
+    public static void main(String[] args) {
+        ListNode headA = new ListNode(4);;
+        ListNode headB = new ListNode(5);;
+
+        ListNode A1 = new ListNode(1);
+        headA.next = A1;
+
+        ListNode A2 = new ListNode(8);;
+        A1.next = A2;
+
+        ListNode A3 = new ListNode(4);;
+        A2.next = A3;
+
+        ListNode A4 = new ListNode(5);;
+        A3.next = A4;
+
+        ListNode B1 = new ListNode(6);;
+        headB.next = B1;
+
+        ListNode B2 = new ListNode(1);;
+        B1.next = B2;
+
+        B2.next = A2;
+
+
+        ListNode pA = getIntersectionNode(headA, headB);
+        System.out.println(pA.val);
 
     }
 }
+
+
+
+
