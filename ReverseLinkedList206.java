364713363959c228c499107fe87ae18f16d64814
Cagan <cagansvncn@gmail.com>
2021-06-08 15:44:49 -0700

    Update ReverseLinkedList206.java

--- ReverseLinkedList206.java
+++ ReverseLinkedList206.java
@@ -13,16 +13,13 @@ public class ReverseLinkedList206 {
     }
 
     public static ListNode reverseList(ListNode head) {
-        ListNode prev = null;
-        ListNode curr = head;
-
-        while (curr != null){
-           ListNode nextTemp = curr.next;
-           curr.next = prev;
-           prev = curr;
-           curr = nextTemp;
-        }
-        return prev;
+       if(head == null || head.next == null){
+           return head;
+       }
+       ListNode p = reverseList(head.next);
+       head.next.next = head;
+       head.next = null;
+       return p;
     }
 
     public static void main(String[] args) {
