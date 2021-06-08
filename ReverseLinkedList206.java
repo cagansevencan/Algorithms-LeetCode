7e533890c1cc0aa6345ae82e0f24790ffe28cc1c
Cagan <cagansvncn@gmail.com>
2021-06-08 14:48:14 -0700

    Create ReverseLinkedList206.java

---
+++ ReverseLinkedList206.java
@@ -0,0 +1,14 @@
+public class ReverseLinkedList206 {
+    static class ListNode {
+        int val;
+        IntersectionTwoLinkedLists160HashTable.ListNode next;
+        ListNode() {}
+        ListNode(int val) {
+            this.val = val;
+        }
+        ListNode(int val, IntersectionTwoLinkedLists160HashTable.ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
