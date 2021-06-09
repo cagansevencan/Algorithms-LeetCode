aceb83f059867848b7134d20eddc5a2ee699902d
Cagan <cagansvncn@gmail.com>
2021-06-08 22:55:49 -0700

    Create OddEvenLinkedList328.java

---
+++ OddEvenLinkedList328.java
@@ -0,0 +1,20 @@
+public class OddEvenLinkedList328 {
+    static class ListNode {
+        int val;
+        AddTwoNumbers2.ListNode next;
+        ListNode() {}
+        ListNode(int val) {
+            this.val = val;
+        }
+        ListNode(int val, AddTwoNumbers2.ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+
+    public ListNode oddEvenList(ListNode head) {
+
+        if(head)
+    }
+}
