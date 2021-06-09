e5509f0cf840ae088feba2f5cbbd8c13a99fc17a
Cagan <cagansvncn@gmail.com>
2021-06-08 23:12:38 -0700

    Update OddEvenLinkedList328.java

--- OddEvenLinkedList328.java
+++ OddEvenLinkedList328.java
@@ -1,20 +1,55 @@
 public class OddEvenLinkedList328 {
     static class ListNode {
         int val;
-        AddTwoNumbers2.ListNode next;
+        ListNode next;
         ListNode() {}
         ListNode(int val) {
             this.val = val;
         }
-        ListNode(int val, AddTwoNumbers2.ListNode next) {
+        ListNode(int val, ListNode next) {
             this.val = val;
             this.next = next;
         }
     }
+    public static ListNode oddEvenList(ListNode head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode even = head;
+        ListNode odd = head.next;
 
+        ListNode oddHead = odd;
 
-    public ListNode oddEvenList(ListNode head) {
+        while (odd != null && odd.next != null){
+            even.next = odd.next;
+            even = odd.next;
+            odd.next = even.next;
+            odd = even.next;
+        }
+        even.next = oddHead;
+        return head;
+    }
 
-        if(head)
+    public static void main(String[] args) {
+        ListNode dummyHead = new ListNode(0);
+
+        ListNode A1 = new ListNode(1);
+        dummyHead.next = A1;
+
+        ListNode A2 = new ListNode(2);
+        A1.next = A2;
+
+        ListNode A3 = new ListNode(3);
+        A2.next = A3;
+
+        ListNode A4 = new ListNode(4);
+        A3.next = A4;
+
+        ListNode A5 = new ListNode(5);
+        A4.next = A5;
+
+        A5.next = null;
+
+        oddEvenList(dummyHead);
     }
 }
