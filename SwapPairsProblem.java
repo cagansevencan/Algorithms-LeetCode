ad3e3d019a37b15a5515b295b8fc0dadce1b88d3
Cagan <cagansvncn@gmail.com>
2021-06-21 12:10:15 -0700

    Update SwapPairsProblem.java

--- SwapPairsProblem.java
+++ SwapPairsProblem.java
@@ -7,7 +7,35 @@ public class SwapPairsProblem {
       ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  }
 
+    //Iterative Approach
     public static ListNode swapPairs(ListNode head) {
+        // Dummy node acts as the prevNode for the head node
+        // of the list and hence stores pointer to the head node.
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+
+        ListNode prevNode = dummy;
+
+        while((head != null)  && (head.next != null)){
+            //Nodes to be swapped
+            ListNode firstNode = head;
+            ListNode secondNode = head.next;
+
+            //Swapping
+            prevNode.next = secondNode;
+            firstNode.next = secondNode.next;
+            secondNode.next = firstNode;
+
+            //Reinitializing the head and prevNode for next swap
+            prevNode = firstNode;
+            head = firstNode.next; // jump
+        }
+        //Return the new head node
+        return dummy.next;
+    }
+
+   //Recursive Step
+   /* public static ListNode swapPairs(ListNode head) {
         //if list has no node or 1 node left
         if(head ==null || head.next == null){
             return head;
@@ -23,7 +51,7 @@ public class SwapPairsProblem {
 
         //Head is the second node now
         return secondNode;
-    }
+    }*/
 
     public static void append(ListNode head, int new_data)
     {
