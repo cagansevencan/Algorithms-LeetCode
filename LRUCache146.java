3494ae363055c794851db4821a20e020c090f663
Cagan <cagansvncn@gmail.com>
2021-06-22 14:17:01 -0700

    Update LRUCache146.java

--- LRUCache146.java
+++ LRUCache146.java
@@ -89,5 +89,10 @@ public class LRUCache146 {
         LRUCache.put(2,2);
         LRUCache.get(1);
         LRUCache.put(3,3); //LRU key was 2, evicts key 2 cache is {1=1, 3=3}
+        LRUCache.get(2); // returns -1 (not found)
+        LRUCache.put(4,4); //LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+        LRUCache.get(1); // returns -1 (not found)
+        LRUCache.get(3); // return 3
+        LRUCache.get(4); // return 4
     }
 }
