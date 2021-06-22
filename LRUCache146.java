14e67f648c138a0416ac466433a58112acedd0e5
Cagan <cagansvncn@gmail.com>
2021-06-21 23:41:38 -0700

    Update LRUCache146.java

--- LRUCache146.java
+++ LRUCache146.java
@@ -1,39 +1,93 @@
-import java.util.LinkedHashMap;
+import java.util.HashMap;
 import java.util.Map;
 
 public class LRUCache146 {
-    //Using LinkedHashMap
-    class LRUCache extends LinkedHashMap<Integer, Integer> {
-        private int capacity;
 
-        public LRUCache(int capacity) {
-            super(capacity, 0.75F, true);
+    class Node {
+        public int key;
+        public int value;
+        public Node prev;
+        public Node next;
+
+        public Node(int key, int value){
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+        private int capacity;
+        Node evictionQueueHead;
+        Node evictionQueueTail;
+        Map<Integer, Node> nodes;
+
+        public LRUCache146(int capacity) {
             this.capacity = capacity;
+            nodes = new HashMap<Integer, Node>();
         }
 
+        private void evict(Node node){
+            if(node.prev != null){
+                node.prev.next = node.next;
+            }
+            if(node.next != null){
+                node.next.prev = node.prev;
+            }
+            if(node == evictionQueueTail){
+                evictionQueueTail = evictionQueueTail.prev;
+            }
+            if(node == evictionQueueHead){
+                evictionQueueHead = evictionQueueHead.next;
+            }
+        }
+
+        private void addToEvictionQueue(Node node){
+            if(evictionQueueTail == null){
+                evictionQueueTail = evictionQueueHead = node;
+                return;
+            }
+            evictionQueueTail.next = node;
+            node.prev = evictionQueueTail;
+            node.next = null;
+            evictionQueueTail = evictionQueueTail.next;
+        }
+
+
         public int get(int key) {
-            return super.getOrDefault(key, -1);
+           if(!nodes.containsKey(key)){
+               return -1;
+           }
+           Node node = nodes.get(key);
+           evict(node);
+           addToEvictionQueue(node);
+           return node.value;
         }
 
         public void put(int key, int value) {
-            super.put(key, value);
+            //If there's no more space in list
+            if(!nodes.containsKey(key) && nodes.size() >= capacity){
+                //We need to evict head of queue first.
+                nodes.remove(evictionQueueHead.key);
+                evict(evictionQueueHead);
+            }
+
+            Node node = null;
+            if(nodes.containsKey(key)){
+                evict(nodes.get(key));
+                node = nodes.get(key);
+                node.value = value;
+            }else{
+                node = new Node(key, value);
+                nodes.put(key, node);
+            }
+            addToEvictionQueue(node);
         }
 
-        //Params:
-        //eldest – The least recently inserted entry in the map,
-        // or if this is an access-ordered map, the least recently accessed entry.
-        // This is the entry that will be removed it this method returns true.
-        // If the map was empty prior to the put or putAll invocation resulting in this invocation,
-        // this will be the entry that was just inserted; in other words,
-        // if the map contains a single entry, the eldest entry is also the newest.
 
-        //Returns:
-        //true if the eldest entry should be removed from the map; false if it should be retained.
-
-        @Override
-        protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest){
-            return size() > capacity;
-        }
-
+    public static void main(String[] args) {
+        LRUCache146 LRUCache = new LRUCache146(2);
+        LRUCache.put(1,1);
+        LRUCache.put(2,2);
+        LRUCache.get(1);
+        LRUCache.put(3,3); //LRU key was 2, evicts key 2 cache is {1=1, 3=3}
     }
 }
