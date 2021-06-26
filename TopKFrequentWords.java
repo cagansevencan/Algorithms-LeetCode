cd026f29f0e713a86d256bd38e1426cff36c6823
Cagan <cagansvncn@gmail.com>
2021-06-25 20:56:27 -0700

    Create TopKFrequentWords.java

---
+++ TopKFrequentWords.java
@@ -0,0 +1,21 @@
+import java.util.Hashtable;
+import java.util.List;
+
+public class TopKFrequentWords {
+
+    public Hashtable calculate_counts(String words){
+
+    }
+
+
+    public List<String> topKFrequent(String[] words, int k) {
+        int word_to_counts = calculate_counts(words);  //{word: count}
+        // [(count, word), (count2, word2), ....]
+
+        for words in word_to_counts:
+
+    }
+
+
+
+}
