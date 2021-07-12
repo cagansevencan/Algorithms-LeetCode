c162ec9b64c158f6533c2fcdd40610128e1e6ded
Cagan <cagansvncn@gmail.com>
2021-07-12 13:42:23 -0700

    Update KClosestToOrigin973.java

--- KClosestToOrigin973.java
+++ KClosestToOrigin973.java
@@ -1,7 +1,30 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class KClosestToOrigin973 {
     public int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<int[]> closest = new PriorityQueue<int[]>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] first, int[] sec) {
+                return calculateDistance(sec) - calculateDistance(first);
+            }
+        });
 
+        for(int[] point : points){
+            closest.add(point);
+            if(closest.size() > k){
+                closest.poll();
+            }
+        }
+        int [][] results = new int[k][2];
+        while(k > 0){
+            results[--k] = closest.poll();
+        }
+        return results;
     }
 
+    private int calculateDistance(int [] point){
+        return point[0] * point[0] + point[1] * point[1];
+    }
 
 }
