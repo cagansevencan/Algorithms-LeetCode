210d25a010d8db015735f45c4a6cb420ed34fde6
Cagan <cagansvncn@gmail.com>
2021-07-12 20:04:18 -0700

    Update MinStack155.java

--- MinStack155.java
+++ MinStack155.java
@@ -3,21 +3,27 @@ import java.util.Stack;
 public class MinStack155 {
 
     private Stack<Integer> stack = new Stack<>();
-    private Stack<Integer> minStack = new Stack<>();
+    private Stack<int[]> minStack = new Stack<>();
 
     /** initialize your data structure here. */
     public MinStack155() {}
 
     public void push(int val) {
         stack.push(val);
-        if(minStack.isEmpty() || val <= minStack.peek()){
-            minStack.push(val);
+
+        if(minStack.isEmpty() || val < minStack.peek()[0]){
+            minStack.push(new int[]{val, 1});
+        }else if(val == minStack.peek()[0]){
+           minStack.peek()[1]++;
         }
     }
 
     public void pop() {
-        if(getMin() == top()) {
-            minStack.pop();
+        if(stack.peek().equals(minStack.peek()[0])){
+            minStack.peek()[1]--;
+        }
+        if(minStack.peek()[1] == 0){
+             minStack.pop();
         }
         stack.pop();
     }
@@ -27,7 +33,7 @@ public class MinStack155 {
     }
 
     public int getMin() {
-       return minStack.peek();
+       return minStack.peek()[0];
     }
 
     public static void main(String[] args) {
@@ -35,9 +41,9 @@ public class MinStack155 {
         minStack.push(-2);
         minStack.push(0);
         minStack.push(-3);
-        minStack.getMin(); // return -3
+        int min = minStack.getMin(); // return -3
         minStack.pop();
         minStack.top();    // return 0
-        minStack.getMin(); // return -2
+        min = minStack.getMin(); // return -2
     }
 }
