10039b6c01edc49f10b95bac5d1f5f42bc0943fe
Cagan <cagansvncn@gmail.com>
2021-07-13 15:24:36 -0700

    Create BrickWall554.java

---
+++ BrickWall554.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+import java.util.List;
+
+public class BrickWall554 {
+    public int leastBricks(List<List<Integer>> wall) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        //Iterate thru each row within the wall
+
+        if(wall.isEmpty()){
+            return 0;
+        }
+
+    }
+}
