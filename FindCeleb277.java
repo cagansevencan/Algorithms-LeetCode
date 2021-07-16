329e502333bad13e1d39b9b7aac675c6222871ac
Cagan <cagansvncn@gmail.com>
2021-07-16 15:43:07 -0700

    Update FindCeleb277.java

--- FindCeleb277.java
+++ FindCeleb277.java
@@ -1,20 +1,24 @@
+/*
 public class FindCeleb277 {
     public class Solution extends Relation {
         public int findCelebrity(int n) {
-            int left = 0;;
-            int right = n-1;
-            while(left < right){
-                if(knows(left, right)){
+            int left = 0;
+            ;
+            int right = n - 1;
+            while (left < right) {
+                if (knows(left, right)) {
                     left++;
-                }else{
+                } else {
                     right--;
                 }
             }
-            for(int i = 0; i < n; i++){
-                if(i != left && (!knows(i, left) || knows(left, i))){
+            for (int i = 0; i < n; i++) {
+                if (i != left && (!knows(i, left) || knows(left, i))) {
                     return -1;
                 }
             }
             return left;
         }
+    }
 }
+ */
