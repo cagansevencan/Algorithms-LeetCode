83f47cb05c481e2b0251e042d7ffd6e7c98df68f
Cagan <cagansvncn@gmail.com>
2021-07-16 17:18:58 -0700

    Update SimplifyPath.java

--- SimplifyPath.java
+++ SimplifyPath.java
@@ -1,5 +1,20 @@
 public class SimplifyPath {
     public static String simplifyPath(String path) {
+        int j = path.lastIndexOf("/");
+        int i = path.lastIndexOf("/", j-1) ;
 
+        String newPath;
+        newPath = path.substring(i, j);
+
+        String lastString = newPath.replace("..", "");
+
+        return lastString;
+    }
+
+    public static void main(String[] args) {
+        String s=  "/../";
+        String a;
+        a = simplifyPath(s);
+        System.out.println(a);
     }
 }
