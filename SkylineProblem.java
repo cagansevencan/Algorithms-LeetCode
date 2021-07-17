ba9e022f65458bce5c61b2052aac223c7ce93127
Cagan <cagansvncn@gmail.com>
2021-07-16 18:09:50 -0700

    Create SkylineProblem.java

---
+++ SkylineProblem.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class SkylineProblem {
+    public static List<int[]> getSkyline(int[][] buildings) {
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
