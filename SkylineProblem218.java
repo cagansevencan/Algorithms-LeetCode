d67bb84fafa7be9780dfc7b1c6f52542aff52cd5
Cagan <cagansvncn@gmail.com>
2021-07-16 18:22:29 -0700

    Create SkylineProblem218.java

---
+++ SkylineProblem218.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import java.util.List;
+
+public class SkylineProblem218 {
+    public static List<int[]> getSkyline(int[][] buildings) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i =0; i< buildings.length; i++){
+            for(int j = 0; j < buildings[i].length; j++){
+                buildings[][]
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
